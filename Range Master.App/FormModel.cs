e5b7dfc336de7006db5c6e978700fe583dabf0de
Matt Wagner <mattgwagner@gmail.com>
2016-09-13 10:26:40 -0400

    Initial revision

---
+++ Range Master.App/FormModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RangeMaster.App
+{
+    public class FormModel
+    {
+        public String IdCode { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Today;
+
+        public String Unit { get; set; }
+
+        public String Evaluator { get; set; }
+
+        public String Remarks { get; set; }
+
+        public Boolean QualifiedWithIba { get; set; } = false;
+
+        public ICollection<Target> Table1 { get; set; } = new List<Target>(capacity: 20);
+
+        public ICollection<Target> Table2 { get; set; } = new List<Target>(capacity: 10);
+
+        public ICollection<Target> Table3 { get; set; } = new List<Target>(capacity: 10);
+
+        public TableResult Column1_Result
+        {
+            get
+            {
+                return new TableResult(Table1.Where(_ => _.Number < 11));
+            }
+        }
+
+        public TableResult Column2_Result
+        {
+            get
+            {
+                return new TableResult(Table1.Where(_ => _.Number > 10));
+            }
+        }
+
+        public TableResult Table1_Result { get { return new TableResult(Table1); } }
+
+        // Table2_Result is also for Column3
+
+        public TableResult Table2_Result { get { return new TableResult(Table2); } }
+
+        // Table3_Result is also for Column 4
+
+        public TableResult Table3_Result { get { return new TableResult(Table3); } }
+
+        public int TotalScore
+        {
+            get
+            {
+                return new[] { Table1_Result, Table2_Result, Table3_Result }.Sum(_ => _.TotalHit);
+            }
+        }
+
+        public Qualification Qualification
+        {
+            get
+            {
+                if (TotalScore >= (int)Qualification.Expert) return Qualification.Expert;
+
+                if (TotalScore >= (int)Qualification.Sharpshooter) return Qualification.Sharpshooter;
+
+                if (TotalScore >= (int)Qualification.Marksman) return Qualification.Marksman;
+
+                return Qualification.Unqualified;
+            }
+        }
+
+        public class Target
+        {
+            public int Number { get; set; }
+
+            public TargetResult Result { get; set; } = TargetResult.NoFire;
+        }
+
+        public class TableResult
+        {
+            public int TotalHit { get; }
+
+            public int TotalMiss { get; }
+
+            public int TotalNoFire { get; }
+
+            public TableResult(IEnumerable<Target> results)
+            {
+                TotalHit = results.Where(_ => _.Result == TargetResult.Hit).Count();
+                TotalMiss = results.Where(_ => _.Result == TargetResult.Miss).Count();
+                TotalNoFire = results.Where(_ => _.Result == TargetResult.NoFire).Count();
+            }
+        }
+    }
+
+    public enum TargetResult
+    {
+        NoFire,
+
+        Hit,
+
+        Miss
+    }
+
+    public enum Qualification
+    {
+        Unqualified,
+
+        Marksman = 23,
+
+        Sharpshooter = 30,
+
+        Expert = 36
+    }
+}
---
+++ Range Master.App/Program.cs
@@ -0,0 +1,124 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Range_Master.App
+{
+    internal class Program
+    {
+        private static string field_prefix { get; } = "form1[0].Page1[0]";
+
+        private static void Main(string[] args)
+        {
+            // Who needs the owner password?
+
+            PdfReader.unethicalreading = true;
+
+            using (var output = new MemoryStream())
+            using (var input = Assembly.GetExecutingAssembly().GetManifestResourceStream("RangeMaster.App.DA_3595-R_Record_Fire_Scorecard.pdf"))
+            using (var reader = new PdfReader(input))
+            using (var stamper = new PdfStamper(reader, output))
+            {
+                var form = stamper.AcroFields;
+
+                // Header Info
+
+                // form1[0].Page1[0].DATE[0]
+
+                // form1[0].Page1[0].IDCODE[0]
+                // form1[0].Page1[0].UNIT[0]
+                // form1[0].Page1[0].EVALUATOR[0]
+
+                // form1[0].Page1[0].Remarks[0]
+
+                // A, B are Table 1 - Prone Supported
+                // C is Table 2 - Prone Unsupported
+                // D is Table 3 - Kneeling Unsupported
+
+                // Indivual Targets
+
+                // {field_prefix}.Hit{A,B,C,D}_{1-10}[0]
+                // {field_prefix}.Miss{A,B,C,D}_{1-10}[0]
+                // {field_prefix}.NoFire{A,B,C,D}_{1-10}[0]
+
+                // Totals for each column (A/B/C/D)
+
+                // {field_prefix}.Total_Hit_Column1[0]
+                // {field_prefix}.Total_Miss_Column1[0]
+                // {field_prefix}.Total_NoFire_Column1[0]
+                // {field_prefix}.Total_Hit_Column2[0]
+                // {field_prefix}.Total_Miss_Column2[0]
+                // {field_prefix}.Total_NoFire_Column2[0]
+                // {field_prefix}.Total_Hit_Column3[0]
+                // {field_prefix}.Total_Miss_Column3[0]
+                // {field_prefix}.Total_NoFire_Column3[0]
+                // {field_prefix}.Total_Hit_Column4[0]
+                // {field_prefix}.Total_Miss_Column4[0]
+                // {field_prefix}.Total_NoFire_Column4[0]
+
+                // Totals for the table
+
+                // {field_prefix}.Hit{A,B,C,D}[0]
+                // {field_prefix}.Miss{A,B,C,D}[0]
+                // {field_prefix}.NoFire{A,B,C,D}[0]
+
+                // {field_prefix}.Total_Hit_Table_1[0]
+                // {field_prefix}.Total_Hit_Table_2[0]
+                // {field_prefix}.Total_Hit_Table_3[0]
+
+                //{field_prefix}.Total_Miss_Table_1[0]
+                //{field_prefix}.Total_Miss_Table_2[0]
+                //{field_prefix}.Total_Miss_Table_3[0]
+
+                // {field_prefix}.Total_NoFire_Table_1[0]
+                // {field_prefix}.Total_NoFire_Table_2[0]
+                // {field_prefix}.Total_NoFire_Table_3[0]
+
+                foreach (var field in form.Fields)
+                {
+                    Console.WriteLine($"{field.Key}");
+                }
+
+                form.SetField($"{field_prefix}.MissA_6[0]", "Test");
+            }
+
+            // Totals for the Entire Iterations
+
+            // {field_prefix}.Total_Points[0]
+
+            // {field_prefix}.Marksman[0]
+            // {field_prefix}.Expert[0]
+            // {field_prefix}.ShrpShot[0]
+            // {field_prefix}.Unqualifd[0]
+
+            // What site did they use?
+
+            //form1[0].Page1[0].AA[0]
+            //form1[0].Page1[0].EE[0]
+            //form1[0].Page1[0].BB[0]
+            //form1[0].Page1[0].GG[0]
+            //form1[0].Page1[0].CC[0]
+            //form1[0].Page1[0].HH[0]
+            //form1[0].Page1[0].DD[0]
+            //form1[0].Page1[0].FF[0]
+
+            // Qualified with IBA?
+
+            //form1[0].Page1[0].YES[0]
+            //form1[0].Page1[0].NO[0]
+
+            // Evaluator Signatures
+
+            //form1[0].Page1[0].Initial1[0]
+            //form1[0].Page1[0].Initial2[0]
+            //form1[0].Page1[0].DATE1[0]
+            //form1[0].Page1[0].DATE2[0]
+
+            //form1[0].Page1[0].signature_BUTTON1[0]
+            //form1[0].Page1[0].signature_BUTTON2[0]
+
+            Console.ReadKey();
+        }
+    }
+}
