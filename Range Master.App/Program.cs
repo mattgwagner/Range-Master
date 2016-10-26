36d72dbe9839fcbe10b6d6637db6819bcb4f0934
Matt Wagner <mattgwagner@gmail.com>
2016-10-25 21:41:25 -0400

    Add basic template, initial code for loading from template

--- Range Master.App/Program.cs
+++ Range Master.App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using RangeMaster.App;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Range_Master.App
 {
@@ -7,15 +9,33 @@ namespace Range_Master.App
     {
         private static void Main(string[] args)
         {
-            var model = new FormModel
+            if (!args.Any())
+            {
+                Console.WriteLine("Usage: RangeMaster.App.exe {input_file}.xls");
+                Environment.Exit(0);
+            }
+
+            foreach (var model in Read_From_Excel(args.First()))
+            {
+                model.ToFile();
+
+                Console.WriteLine($"Saved to {model.FileName}");
+            }
+
+            Console.WriteLine("Completed!");
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<FormModel> Read_From_Excel(String filename)
+        {
+            // TODO actually read the file
+
+            yield return new FormModel
             {
                 SoldierIdentifier = "WAGNER MATTHEW",
                 Unit = "A BTRY 2-116 FA"
             };
-
-            model.ToFile();
-
-            Console.ReadKey();
         }
     }
 }
