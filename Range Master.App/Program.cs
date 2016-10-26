c72d327dde6c2a7ac7611b473fe3a677104b1255
Matt Wagner <mattgwagner@gmail.com>
2016-10-25 21:51:29 -0400

    WIP on parsing the excel file

--- Range Master.App/Program.cs
+++ Range Master.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using RangeMaster.App;
+﻿using LinqToExcel;
+using RangeMaster.App;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,13 +30,47 @@ namespace Range_Master.App
 
         public static IEnumerable<FormModel> Read_From_Excel(String filename)
         {
-            // TODO actually read the file
+            var excel = new ExcelQueryFactory(filename);
 
-            yield return new FormModel
+            foreach (var row in excel.Worksheet("raw").Skip(2))
             {
-                SoldierIdentifier = "WAGNER MATTHEW",
-                Unit = "A BTRY 2-116 FA"
-            };
+                int current = 0;
+
+                var model = new FormModel
+                {
+                    SoldierIdentifier = row[current++],
+                    Unit = row[current++],
+                };
+
+                foreach (var target in Enumerable.Range(1, 20))
+                {
+                    model.Table1.Add(new FormModel.Target
+                    {
+                        Number = target,
+                        Result = row[current++].Cast<int>() == 1 ? TargetResult.Hit : TargetResult.Miss
+                    });
+                }
+
+                foreach (var target in Enumerable.Range(1, 10))
+                {
+                    model.Table2.Add(new FormModel.Target
+                    {
+                        Number = target,
+                        Result = row[current++].Cast<int>() == 1 ? TargetResult.Hit : TargetResult.Miss
+                    });
+                }
+
+                foreach (var target in Enumerable.Range(1, 10))
+                {
+                    model.Table3.Add(new FormModel.Target
+                    {
+                        Number = target,
+                        Result = row[current++].Cast<int>() == 1 ? TargetResult.Hit : TargetResult.Miss
+                    });
+                }
+
+                yield return model;
+            }
         }
     }
 }
